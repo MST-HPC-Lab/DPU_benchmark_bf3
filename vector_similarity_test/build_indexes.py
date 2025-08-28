ea2bec90cf8685a6a02b186cdd49eba7bf9c56e5
Nathan Tibbetts <nathan.paul.tibbetts@gmail.com>
2025-08-28 14:14:58 -0500

    more tests

--- vector_similarity_test/build_indexes.py
+++ vector_similarity_test/build_indexes.py
@@ -4,29 +4,14 @@ import faiss
 from timeit import repeat
 
 
-# Load Data File
-# colnames = ["VOCAB"] + [str(i) for i in range(200)]
-df = pd.read_csv("../Data/glove.6B.200d.txt", sep=" ", quoting=3, header=None)
-d = 200
-k = 10
-
-# Split into vocab column and data
-vocab = df.iloc[:,0]
-df = df.drop(0, axis=1)
-
-# Testing vs. Training Split
-test_i = [i for i in range(199,400000,200)] # len is ~2000
-train_i = [i for i in range(400000) if (i+199)%200]
-x_query = df.iloc[test_i]
-x_train = df.iloc[train_i]
 
 # Recall Measurement functions
 truth_D, truth_I = None, None
-def recall_at_k(test, truth, k=k):
+def recall_at_k(test, truth, k):
     assert len(test) == k
     assert len(truth) == k
     return len(set(truth).intersection(test)) / k
-def batch_recall(test_I, truth_I, k=k):
+def batch_recall(test_I, truth_I, k):
     results = []
     for i, row in enumerate(test_I):
         results.append(recall_at_k(row, truth_I[i], k))
@@ -45,27 +30,21 @@ def brute_force_build():
 
 truth_I = None
 truth_D = None
-def brute_force_search(k=k, measure_accuracy=False):
+def brute_force_search(k, measure_accuracy=False):
     global FL2, truth_D, truth_I
     truth_D, truth_I = FL2.search(x_query, k) # D: Distances, I: Indices
     if measure_accuracy:
         return 1.0 # Accuracy is perfect because we're treating this as ground truth
 
 # Search function is used for all other indexes
-def search(index, k=k, measure_accuracy=True):
+def search(index, k, measure_accuracy=True):
     # Must run brute_force_search first, for results to compare accuracy against
     D, I = index.search(x_query, k)
     if measure_accuracy:
         global truth_I;
-        return batch_recall(I, truth_I)
+        return batch_recall(I, truth_I, k)
 
 #------------------------------------------
-
-lsh_nbits = 8*d
-pq_nbits = 12
-pq_subquantizers = 40
-ivfpq_ncentroids = 8
-ivfpq_codesize = 40
 
 # LSH Index
 LSH = None
@@ -97,14 +76,17 @@ def ivfpq_build(ncentroids, code_size, n_bits):
 
 #------------------------------------------
 
-def test_suite(r=1): # r = number of repeats to get more accurate average timings
-    recall_str = f"{k}-recall@{k}"
-    print(f"(All times averaged out of {r} repeats)")
+# To time both building and searching
+def test_suite(k=1, r=1):
+    # k = recall set size
+    # r = number of repeats to get more accurate average timings
+    print(f"Measuring: {k}-recall@{k}")
+    print(f"Repeats: {r} (All times averaged across)")
     
     # Brute Force Ground Truth ----------------
     print("---------------------- Brute Force ----------------------")
     bf_btime = np.mean(repeat(lambda: brute_force_build(),  number=1, repeat=r))#, globals=globals()))
-    bf_stime = np.mean(repeat(lambda: brute_force_search(), number=1, repeat=r))#, globals=globals()))
+    bf_stime = np.mean(repeat(lambda: brute_force_search(k), number=1, repeat=r))#, globals=globals()))
 
     print("Brute Force Accuracy: 1.0")
     print("Brute Force Build  Time:", bf_btime)
@@ -174,9 +156,124 @@ def test_suite(r=1): # r = number of repeats to get more accurate average timing
     print("Fastest Search:", indexes[fasts_i])
 
 
+# To build without timing it
+def test_build():
+    brute_force_build()
+    lsh_build(lsh_nbits)
+    pq_build(pq_subquantizers, pq_nbits)
+    ivfpq_build(ivfpq_ncentroids, ivfpq_codesize, pq_nbits)
+
+
+# To time the search only
+def test_search(k=1, r=1, verbose=True):
+    # k = recall set size
+    # r = number of repeats to get more accurate average timings
+    if verbose: print(f"Measuring: {k}-recall@{k}")
+    if verbose: print(f"Repeats: {r} (All times averaged across)")
+    
+    # Brute Force Ground Truth ----------------
+    if verbose: print("---------------------- Brute Force ----------------------")
+    bf_stime = np.mean(repeat(lambda: brute_force_search(k), number=1, repeat=r))#, globals=globals()))
+    if verbose: print("Brute Force avg Search Time:", bf_stime)
+
+
+    # LSH -------------------------------------
+    if verbose: print("---------------------- LSH ----------------------")
+    global LSH, lsh_nbits;
+    if verbose: print("LSH n_bits:", lsh_nbits)
+    lsh_result = search(LSH, k)
+    lsh_stime = np.mean(repeat(lambda: search(LSH, k, measure_accuracy=False),  number=1, repeat=r))
+
+    if verbose: print("LSH recall:", lsh_result)
+    if verbose: print("LSH avg search time:", lsh_stime)
+    if verbose: print(f"LSH search Speedup compared to brute force: {bf_stime/lsh_stime}")
+
+
+    # PQ -------------------------------------
+    if verbose: print("---------------------- PQ ----------------------")
+    global PQ, pq_nbits, pq_subquantizers;
+    if verbose: print("PQ n_bits:", pq_nbits)
+    if verbose: print("PQ Subquantizers:", pq_subquantizers)
+    pq_result = search(PQ, k)
+    pq_stime = np.mean(repeat(lambda: search(PQ, k, measure_accuracy=False),  number=1, repeat=r))
+
+    if verbose: print("PQ recall:", pq_result)
+    if verbose: print("PQ search time:", pq_stime)
+    if verbose: print(f"PQ search Speedup: {bf_stime/pq_stime}")
+
+
+    # IVF PQ -------------------------------------
+    if verbose: print("---------------------- IVF PQ ----------------------")
+    global IVFPQ, ivfpq_ncentroids, ivfpq_codesize;
+    if verbose: print("IVF PQ n_bits:", pq_nbits)
+    if verbose: print("IVF PQ ncentroids:", ivfpq_ncentroids)
+    if verbose: print("IVF PQ codesize:", ivfpq_codesize)
+    ivfpq_result = search(IVFPQ, k)
+    ivfpq_stime = np.mean(repeat(lambda: search(IVFPQ, k, measure_accuracy=False),  number=1, repeat=r))
+
+    if verbose: print("IVFPQ recall:", ivfpq_result)
+    if verbose: print("IVFPQ search time:", ivfpq_stime)
+    if verbose: print(f"IVFPQ search Speedup: {bf_stime/ivfpq_stime}")
+
+    return [bf_stime, lsh_result, lsh_stime, pq_result, pq_stime, ivfpq_result, ivfpq_stime]
+
+
+#===============================================================================
+
 
 if __name__ == "__main__":
-    test_suite(r=1)
+    # Load Data File
+    # colnames = ["VOCAB"] + [str(i) for i in range(200)]
+    filename = "../Data/glove.6B.200d.txt"
+    df = pd.read_csv(filename, sep=" ", quoting=3, header=None)
+
+    # Split into vocab column and data
+    vocab = df.iloc[:,0]
+    df = df.drop(0, axis=1)
+    d = len(df.iloc[0])
+    print(f'File: "{filename}"')
+    print("Dimensions:", d)
+
+    # Testing vs. Training Split
+    test_i = [i for i in range(199,400000,200)] # len is ~2000
+    train_i = [i for i in range(400000) if (i+199)%200] # len is ~398000
+    x_query = df.iloc[test_i]
+    x_train = df.iloc[train_i]
+    print("Train Size:", len(test_i))
+    print("Test  Size:", len(train_i))
+
+    # # Presets based on 200 dimensions and best results with k=10
+    # lsh_nbits = 8*d
+    # pq_nbits = 12
+    # pq_subquantizers = 40
+    # ivfpq_ncentroids = 8
+    # ivfpq_codesize = 40
+
+    # Presets based on 200 dimensions and more efficient results, fastest that is still perfect with k=1, though it ends up being poor with k=10
+    lsh_nbits = 2*d
+    pq_nbits = 8
+    pq_subquantizers = 20
+    ivfpq_ncentroids = 10
+    ivfpq_codesize = 20
+
+    # Run Full Test
+    # test_suite(k=1, r=1)
+
+    # Run search timing test with various k
+    print("Building...\r", end='')
+    test_build()
+    results = []
+    for k in [1, 2, 3, 5, 7, 10, 25, 50, 75, 100]:
+        print(f"Searching with k={k}...\r", end='')
+        results.append(test_search(k=10, r=3, verbose=False))
+    bf_time, lsh_recall, lsh_time, pq_recall, pq_time, ivfpq_recall, ivfpq_time = zip(*results)
+    print("Brute Force Time:", bf_time)
+    print("LSH Recall:", lsh_recall)
+    print("LSH Time:", lsh_time)
+    print("PQ Recall:", pq_recall)
+    print("PQ Time:", pq_time)
+    print("IVFPQ Recall:", ivfpq_recall)
+    print("IVFPQ Time:", ivfpq_time)
 
 """
 from datasketch import MinHash
---
+++ vector_similarity_test/graph_recall_vs_speed.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# DATA : All data comes from bf3host_log_k10_multi.txt
+host_LSH_k10_recall   =   np.array([0.1814, 0.2716, 0.3422, 0.4014, 0.4389])
+host_LSH_k10_time     =   np.array([0.09369638, 0.07801421, 0.10790243, 0.16929569, 0.24661489]) # All these are Search times, not build
+
+host_PQ_k10_recall    =  np.array([0.1478, 0.2013, 0.2346, 0.3877, 0.4508, 0.6049,
+                          0.2206, 0.2928, 0.3523, 0.5483, 0.6104, 0.7718])
+host_PQ_k10_time      =  np.array([0.04935188, 0.0631966,  0.08354286, 0.13346617, 0.2258872,  0.25608645,
+                          0.28311777, 0.45357781, 0.56533347, 1.10801453, 1.34134911, 2.37059066])
+
+host_IVFPQ_k10_recall = np.array([0.1318, 0.2096, 0.2439, 0.3875, 0.4428, 0.604 ,
+                                  0.1326, 0.2103, 0.2444, 0.3885, 0.4448, 0.6019,
+                                  0.137,  0.2161, 0.248,  0.3918, 0.439,  0.6054,
+                                  0.1407, 0.2216, 0.2549, 0.3917, 0.4513, 0.6114,
+                                  0.1481, 0.2279, 0.2605, 0.3957, 0.451,  0.6068,
+                                  0.1512, 0.2244, 0.2555, 0.388,  0.4396, 0.5823,
+                                  0.1937, 0.3178, 0.3696, 0.5593, 0.6228, 0.771 ,
+                                  0.1962, 0.3275, 0.3771, 0.5557, 0.6199, 0.7744,
+                                  0.1992, 0.3111, 0.3584, 0.5489, 0.6228, 0.7719,
+                                  0.2029, 0.3118, 0.3577, 0.556,  0.6164, 0.7707,
+                                  0.2031, 0.3123, 0.3662, 0.548,  0.6081, 0.7591,
+                                  0.2058, 0.3197, 0.3557, 0.5253, 0.5978, 0.7249])
+host_IVFPQ_k10_time   = np.array([0.05645957, 0.06282316, 0.12887639, 0.13357753, 0.18787236, 0.26135121,
+                                  0.05581867, 0.06276742, 0.12734022, 0.13166853, 0.18626516, 0.26474065,
+                                  0.05321883, 0.05936221, 0.12136302, 0.12444651, 0.17543829, 0.24981729,
+                                  0.03967796, 0.0445958,  0.08797821, 0.0931378,  0.13095679, 0.18460435,
+                                  0.03087261, 0.03473735, 0.06838999, 0.07319922, 0.10356212, 0.14516545,
+                                  0.01884667, 0.02106053, 0.04103,    0.04425134, 0.06042092, 0.08578111,
+                                  0.29340505, 0.53759353, 0.66087242, 1.31016324, 1.60060616, 2.64152055,
+                                  0.2936734,  0.537353,   0.65940813, 1.30188058, 1.63428097, 2.63212109,
+                                  0.27960513, 0.50757042, 0.62711092, 1.23178991, 1.50458103, 2.47118232,
+                                  0.20828176, 0.38131106, 0.4683668,  0.99884254, 1.125868,   1.78082262,
+                                  0.1627288,  0.29789343, 0.36957798, 0.72049397, 0.86976058, 1.41725447,
+                                  0.0982186,  0.17936766, 0.22088947, 0.42851132, 0.51496834, 0.84599951])
+
+# NOTE: This data was done with a test size of 1000; thus we could simply use these numbers as miliseconds instead of seconds per thousand;
+#   HOWEVER, instead we will use queries per second (1000 / times)
+
+# GRAPHS :
+plt.title("Time Cost of Recall Across Methods (varying parameters)")
+plt.grid(color='0.8')
+# plt.scatter(host_IVFPQ_k10_recall, host_IVFPQ_k10_time, color='orange')
+# plt.scatter(host_PQ_k10_recall, host_PQ_k10_time, color='teal')
+# plt.scatter(host_LSH_k10_recall, host_LSH_k10_time, color='purple')
+# plt.ylabel("Time (ms)")
+plt.scatter(host_IVFPQ_k10_recall, 1000/host_IVFPQ_k10_time, color='orange')
+plt.scatter(host_PQ_k10_recall, 1000/host_PQ_k10_time, color='teal')
+plt.scatter(host_LSH_k10_recall, 1000/host_LSH_k10_time, color='purple')
+plt.ylabel("Queries per Second (1/s)")
+plt.xlabel("10-Recall@10")
+plt.xlim(0, 1.1)
+plt.legend(["IVFPQ", "PQ", "LSH"])
+# plt.show()#block=False)
+plt.savefig("results/recall_vs_speed.png")
+plt.close()
+
+# Logarithmic Graph:
+plt.title("Time Cost of Recall Across Methods (varying parameters)")
+plt.grid(color='0.8')
+plt.loglog(host_IVFPQ_k10_recall, 1000/host_IVFPQ_k10_time, 'o', color='orange')
+plt.loglog(host_PQ_k10_recall, 1000/host_PQ_k10_time, 'o', color='teal')
+plt.loglog(host_LSH_k10_recall, 1000/host_LSH_k10_time, 'o', color='purple')
+plt.ylabel("Queries per Second (1/s)")
+plt.xlabel("10-Recall@10")
+plt.xlim(0, 1.1)
+plt.legend(["IVFPQ", "PQ", "LSH"])
+# plt.show()#block=False)
+plt.savefig("results/recall_vs_speed_logarithmic.png")
+plt.close()
