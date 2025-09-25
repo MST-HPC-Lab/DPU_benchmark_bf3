a589549763440f1be4f0589b705480c2eb65762b
arjun kirubakaran <arjunkiruba1357@gmail.com>
2025-09-25 14:40:51 -0500

    made chnages to graph by adding hnsw

--- vector_similarity_test/graph_recall_vs_speed.py
+++ vector_similarity_test/graph_recall_vs_speed.py
@@ -36,6 +36,26 @@ host_IVFPQ_k10_time   = np.array([0.05645957, 0.06282316, 0.12887639, 0.13357753
                                   0.1627288,  0.29789343, 0.36957798, 0.72049397, 0.86976058, 1.41725447,
                                   0.0982186,  0.17936766, 0.22088947, 0.42851132, 0.51496834, 0.84599951])
 
+host_HNSW_k10_recall = np.array([0.3393, 0.47, 0.5238, 0.5611, 0.5973,
+                                 0.3481, 0.51, 0.5755, 0.6238, 0.6682,
+                                 0.3756, 0.5427, 0.6182, 0.6811, 0.7238,
+                                 0.3879, 0.5414, 0.5877, 0.6317, 0.6654,
+                                 0.4159, 0.5862, 0.6416, 0.6881, 0.7232,
+                                 0.4344, 0.6317, 0.6943, 0.7395, 0.7736,
+                                 0.4311, 0.5771, 0.6285, 0.6724, 0.7027,
+                                 0.4704, 0.635, 0.6847, 0.7278, 0.7567,
+                                 0.501, 0.6686, 0.7225, 0.769,  0.7974])
+
+
+host_HNSW_k10_time = np.array([0.00440523, 0.00730198, 0.01104281, 0.01787551, 0.03169211,
+                               0.00261001, 0.0052271,  0.00845109, 0.01445478, 0.02708711,
+                               0.0028165,  0.00579303, 0.00984604, 0.01755209, 0.03132006,
+                               0.00534694, 0.01084924, 0.01520071, 0.02467889, 0.04334835,
+                               0.00412655, 0.00949688, 0.01564161, 0.02632121, 0.04719561,
+                               0.00363066, 0.00905407, 0.01576778, 0.0271121, 0.05134012,
+                               0.00749607, 0.01310064, 0.01729034, 0.02896699, 0.05140728,
+                               0.00742654, 0.01511715, 0.02221897, 0.03797912, 0.06963505,
+                               0.00612769, 0.01543398, 0.02560011, 0.0434842,  0.0774608])
 # NOTE: This data was done with a test size of 1000; thus we could simply use these numbers as miliseconds instead of seconds per thousand;
 #   HOWEVER, instead we will use queries per second (1000 / times)
 
@@ -45,14 +65,16 @@ plt.grid(color='0.8')
 # plt.scatter(host_IVFPQ_k10_recall, host_IVFPQ_k10_time, color='orange')
 # plt.scatter(host_PQ_k10_recall, host_PQ_k10_time, color='teal')
 # plt.scatter(host_LSH_k10_recall, host_LSH_k10_time, color='purple')
+
 # plt.ylabel("Time (ms)")
 plt.scatter(host_IVFPQ_k10_recall, 1000/host_IVFPQ_k10_time, color='orange')
 plt.scatter(host_PQ_k10_recall, 1000/host_PQ_k10_time, color='teal')
 plt.scatter(host_LSH_k10_recall, 1000/host_LSH_k10_time, color='purple')
+plt.scatter(host_HNSW_k10_recall, 1000/host_HNSW_k10_time, color='navy')
 plt.ylabel("Queries per Second (1/s)")
 plt.xlabel("10-Recall@10")
 plt.xlim(0, 1.1)
-plt.legend(["IVFPQ", "PQ", "LSH"])
+plt.legend(["IVFPQ", "PQ", "LSH", "HNSW"])
 # plt.show()#block=False)
 plt.savefig("results/recall_vs_speed.png")
 plt.close()
@@ -63,10 +85,11 @@ plt.grid(color='0.8')
 plt.loglog(host_IVFPQ_k10_recall, 1000/host_IVFPQ_k10_time, 'o', color='orange')
 plt.loglog(host_PQ_k10_recall, 1000/host_PQ_k10_time, 'o', color='teal')
 plt.loglog(host_LSH_k10_recall, 1000/host_LSH_k10_time, 'o', color='purple')
+plt.loglog(host_HNSW_k10_recall, 1000/host_HNSW_k10_time, 'o', color='navy')
 plt.ylabel("Queries per Second (1/s)")
 plt.xlabel("10-Recall@10")
 # plt.xlim(0, 1.1)
-plt.legend(["IVFPQ", "PQ", "LSH"])
+plt.legend(["IVFPQ", "PQ", "LSH", "HNSW"])
 # plt.show()#block=False)
 plt.savefig("results/recall_vs_speed_logarithmic.png")
 plt.close()
