d5bfe7f22e00c94f3ee7058b4e04b37c44ada163
Nathan Tibbetts <nathan.paul.tibbetts@gmail.com>
2026-06-03 14:56:10 -0500

    debug

--- Data/mat_to_txt.py
+++ Data/mat_to_txt.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     # Convert input file name from .mat to .txt with header and index column
     mat_data = loadmat(argv[1])['fea']
-    indexes = np.arange(mat_data.shape[1]).reshape(-1, 1)
+    indexes = np.arange(mat_data.shape[0]).reshape(-1, 1)
     np.savetxt(argv[1].replace('.mat', '.txt'), np.column_stack([indexes, mat_data]), delimiter=' ', newline='\n')
 
     # TODO: Still needs header line! I did this manually for our set for now.
