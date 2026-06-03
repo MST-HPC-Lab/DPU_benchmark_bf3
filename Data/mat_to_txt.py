cc6751792ce195edb40f7fcac9ff5ea2f7c610ee
Nathan Tibbetts <nathan.paul.tibbetts@gmail.com>
2026-06-03 18:16:07 -0500

    mat

--- Data/mat_to_txt.py
+++ Data/mat_to_txt.py
@@ -6,7 +6,7 @@ from sys import argv
 
 if __name__ == "__main__":
     # Convert input file name from .mat to .txt with header and index column
-    mat_data = loadmat(argv[1])['fea']
+    mat_data = loadmat(argv[1])['fea'].T
     indexes = np.arange(mat_data.shape[0], dtype=np.uint8).reshape(-1, 1)
     np.savetxt(argv[1].replace('.mat', '.txt'), np.column_stack([indexes, mat_data]), delimiter=' ', newline='\n')
 
