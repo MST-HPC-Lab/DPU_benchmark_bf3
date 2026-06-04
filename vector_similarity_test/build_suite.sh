022cd123e2bca00d1f50c307decece476df1e263
Nathan Tibbetts <nathan.paul.tibbetts@gmail.com>
2026-06-03 19:58:56 -0500

    bash script debug

--- vector_similarity_test/build_suite.sh
+++ vector_similarity_test/build_suite.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# ./build_suite.sh & disown
+# nohup ./build_suite.sh &
+# ./build_suite.sh >> results/combined_log.txt 2>>&1 & disown
 if grep -q "BlueField" /sys/class/dmi/id/product_name; then
     echo "DETECTED BLUEFIELD; DISCONTINUING!"
 
@@ -18,24 +19,24 @@ else
     python3 index_builder.py --file "fasttext_cc.en.300.vec" --out_folder "fasttext_128d_400k" --num_vecs 400000 --dim 128
 
     echo "BUILDING SIFT10M INDEXES"
-    python3 index_builder.py --file "sift10m.txt" --out_folder "sift"
+    python3 index_builder.py --file "sift10m.mat" --out_folder "sift"
 
     echo "BUILDING SIFT10M EQUAL INDEXES"
-    python3 index_builder.py --file "sift10m.txt" --out_folder "sift_128d_400k" --num_vecs 400000 --dim 128
+    python3 index_builder.py --file "sift10m.mat" --out_folder "sift_128d_400k" --num_vecs 400000 --dim 128
 
     echo "BUILDING SIFT10M INCREASING INDEXES 100k"
-    python3 index_builder.py --file "sift10m.txt" --out_folder "sift_100k" --num_vecs 100000
+    python3 index_builder.py --file "sift10m.mat" --out_folder "sift_100k" --num_vecs 100000
     echo "BUILDING SIFT10M INCREASING INDEXES 200k"
-    python3 index_builder.py --file "sift10m.txt" --out_folder "sift_200k" --num_vecs 200000
+    python3 index_builder.py --file "sift10m.mat" --out_folder "sift_200k" --num_vecs 200000
     echo "BUILDING SIFT10M INCREASING INDEXES 500k"
-    python3 index_builder.py --file "sift10m.txt" --out_folder "sift_500k" --num_vecs 500000
+    python3 index_builder.py --file "sift10m.mat" --out_folder "sift_500k" --num_vecs 500000
     echo "BUILDING SIFT10M INCREASING INDEXES 1m"
-    python3 index_builder.py --file "sift10m.txt" --out_folder "sift_1m" --num_vecs 1000000
+    python3 index_builder.py --file "sift10m.mat" --out_folder "sift_1m" --num_vecs 1000000
     echo "BUILDING SIFT10M INCREASING INDEXES 2m"
-    python3 index_builder.py --file "sift10m.txt" --out_folder "sift_2m" --num_vecs 2000000
+    python3 index_builder.py --file "sift10m.mat" --out_folder "sift_2m" --num_vecs 2000000
     echo "BUILDING SIFT10M INCREASING INDEXES 5m"
-    python3 index_builder.py --file "sift10m.txt" --out_folder "sift_5m" --num_vecs 5000000
+    python3 index_builder.py --file "sift10m.mat" --out_folder "sift_5m" --num_vecs 5000000
     echo "BUILDING SIFT10M INCREASING INDEXES 10m"
-    python3 index_builder.py --file "sift10m.txt" --out_folder "sift_10m" --num_vecs 10000000
+    python3 index_builder.py --file "sift10m.mat" --out_folder "sift_10m" --num_vecs 10000000
 
 fi
