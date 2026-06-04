b140ac584c5f53d1d9fe92c4d445b4674de0f9bc
Nathan Tibbetts <nathan.paul.tibbetts@gmail.com>
2026-06-04 14:08:34 -0500

    added update_ground_truth.sh

---
+++ vector_similarity_test/update_ground_truth.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ./update_ground_truth.sh
+
+echo "UPDATING glove GROUND TRUTH"
+python3 index_searcher.py --only "none" --indexes_dir "glove"
+
+echo "UPDATING fasttext GROUND TRUTH"
+python3 index_searcher.py --only "none" --indexes_dir "fasttext"
+
+echo "UPDATING sift GROUND TRUTH"
+python3 index_searcher.py --only "none" --indexes_dir "sift"
+
+echo "UPDATING equalized 128d 400k GROUND TRUTH"
+python3 index_searcher.py --only "none" --indexes_dir "glove_128d_400k"
+python3 index_searcher.py --only "none" --indexes_dir "fasttext_128d_400k"
+python3 index_searcher.py --only "none" --indexes_dir "sift_128d_400k"
+
+echo "UPDATING sift scaling GROUND TRUTH"
+python3 index_searcher.py --only "none" --indexes_dir "sift_100k"
+python3 index_searcher.py --only "none" --indexes_dir "sift_200k"
+python3 index_searcher.py --only "none" --indexes_dir "sift_500k"
+python3 index_searcher.py --only "none" --indexes_dir "sift_1m"
+python3 index_searcher.py --only "none" --indexes_dir "sift_2m"
+python3 index_searcher.py --only "none" --indexes_dir "sift_5m"
+python3 index_searcher.py --only "none" --indexes_dir "sift_10m"
