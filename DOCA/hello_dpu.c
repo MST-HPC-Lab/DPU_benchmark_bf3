fc8b3502ba2b91fa2a679346144a4bf9c4c5849f
Pokharel <sp5rt@rtsg2742jh97f.managed.mst.edu>
2026-06-28 18:54:12 -0500

    corrected

--- DOCA/hello_dpu.c
+++ DOCA/hello_dpu.c
@@ -3,26 +3,19 @@
 #include <doca_log.h>
 #include <doca_error.h>
 
-/* Define application log name */
 DOCA_LOG_REGISTER(HELLO_WORLD);
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     doca_error_t result;
 
-    /* Register a logger to display DOCA messages */
     result = doca_log_backend_create_standard();
     if (result != DOCA_SUCCESS) {
         printf("Failed to register standard log backend\n");
         return EXIT_FAILURE;
     }
 
-    /* Set global log level */
-    doca_log_global_set_level(DOCA_LOG_LEVEL_INFO);
-
     DOCA_LOG_INFO("Hello, DOCA World!");
-
-    /* Standard cleanup */
-    // Free resources here...
 
     return EXIT_SUCCESS;
 }
