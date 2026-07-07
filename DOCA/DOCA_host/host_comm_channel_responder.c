028c9fff8762db6b545d6b9def5721fe6a0afa53
Pokharel <sp5rt@rtsg2742jh97f.managed.mst.edu>
2026-07-07 12:23:13 -0500

    changed code

--- DOCA/DOCA_host/host_comm_channel_responder.c
+++ DOCA/DOCA_host/host_comm_channel_responder.c
@@ -3,94 +3,96 @@
 #include <string.h>
 #include <stdint.h>
 #include <unistd.h>
-#include <errno.h>
-
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 
 #include <doca_error.h>
 #include <doca_log.h>
 #include <doca_dev.h>
 #include <doca_mmap.h>
 #include <doca_types.h>
+#include <doca_comm_channel.h>
 
-DOCA_LOG_REGISTER(HOST_DMA_RESPONDER);
+DOCA_LOG_REGISTER(HOST_CC_RESPONDER);
 
 #define BUFFER_SIZE 1024
-#define TCP_PORT 5555
+#define SERVICE_NAME "dma_blob_service"
 
-struct export_file_header {
+struct export_blob_header {
     uint64_t remote_addr;
     uint64_t remote_len;
     uint64_t export_desc_len;
 };
 
-static int send_all(int fd, const void *buf, size_t len)
+static doca_error_t open_export_pci_device(struct doca_dev **dev)
 {
-    const char *ptr = buf;
+    doca_error_t result;
+    struct doca_devinfo **dev_list;
+    uint32_t nb_devs;
 
-    while (len > 0) {
-        ssize_t sent = send(fd, ptr, len, 0);
-        if (sent <= 0)
-            return -1;
+    result = doca_devinfo_create_list(&dev_list, &nb_devs);
+    if (result != DOCA_SUCCESS)
+        return result;
 
-        ptr += sent;
-        len -= sent;
+    for (uint32_t i = 0; i < nb_devs; i++) {
+        uint8_t supported = 0;
+
+        result = doca_mmap_cap_is_export_pci_supported(dev_list[i], &supported);
+        if (result == DOCA_SUCCESS && supported) {
+            result = doca_dev_open(dev_list[i], dev);
+            if (result == DOCA_SUCCESS) {
+                DOCA_LOG_INFO("Opened PCI export-capable device %u", i);
+                doca_devinfo_destroy_list(dev_list);
+                return DOCA_SUCCESS;
+            }
+        }
     }
 
-    return 0;
+    doca_devinfo_destroy_list(dev_list);
+    return DOCA_ERROR_NOT_FOUND;
 }
 
-static int create_server_socket(void)
+static doca_error_t open_device_representor(struct doca_dev *dev,
+                                            struct doca_dev_rep **dev_rep)
 {
-    int server_fd;
-    int opt = 1;
-    struct sockaddr_in addr;
+    doca_error_t result;
+    struct doca_devinfo_rep **rep_list;
+    uint32_t nb_reps;
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket");
-        return -1;
+    result = doca_devinfo_rep_create_list(dev,
+                                          DOCA_DEVINFO_REP_FILTER_NET,
+                                          &rep_list,
+                                          &nb_reps);
+    if (result != DOCA_SUCCESS)
+        return result;
+
+    if (nb_reps == 0) {
+        doca_devinfo_rep_destroy_list(rep_list);
+        return DOCA_ERROR_NOT_FOUND;
     }
 
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    result = doca_dev_rep_open(rep_list[0], dev_rep);
+    if (result == DOCA_SUCCESS)
+        DOCA_LOG_INFO("Opened device representor 0");
 
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(TCP_PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        perror("bind");
-        close(server_fd);
-        return -1;
-    }
-
-    if (listen(server_fd, 1) < 0) {
-        perror("listen");
-        close(server_fd);
-        return -1;
-    }
-
-    return server_fd;
+    doca_devinfo_rep_destroy_list(rep_list);
+    return result;
 }
 
 int main(void)
 {
     doca_error_t result;
-    struct doca_devinfo **dev_list;
+
     struct doca_dev *dev = NULL;
+    struct doca_dev_rep *dev_rep = NULL;
     struct doca_mmap *mmap = NULL;
-    uint32_t nb_devs;
+
+    struct doca_comm_channel_ep_t *ep = NULL;
+    struct doca_comm_channel_addr_t *peer_addr = NULL;
 
     char *buffer = NULL;
     const void *export_desc = NULL;
     size_t export_desc_len = 0;
 
-    int server_fd = -1;
-    int client_fd = -1;
+    struct export_blob_header hdr;
 
     result = doca_log_backend_create_standard();
     if (result != DOCA_SUCCESS)
@@ -103,25 +105,17 @@ int main(void)
     memset(buffer, 'H', BUFFER_SIZE);
     buffer[BUFFER_SIZE - 1] = '\0';
 
-    result = doca_devinfo_create_list(&dev_list, &nb_devs);
-    if (result != DOCA_SUCCESS)
+    result = open_export_pci_device(&dev);
+    if (result != DOCA_SUCCESS) {
+        DOCA_LOG_ERR("Failed to open export-capable device: %s",
+                     doca_error_get_descr(result));
         return EXIT_FAILURE;
-
-    for (uint32_t i = 0; i < nb_devs; i++) {
-        uint8_t supported = 0;
-
-        result = doca_mmap_cap_is_export_pci_supported(dev_list[i], &supported);
-        if (result == DOCA_SUCCESS && supported) {
-            result = doca_dev_open(dev_list[i], &dev);
-            if (result == DOCA_SUCCESS) {
-                DOCA_LOG_INFO("Opened PCI export-capable device %u", i);
-                break;
-            }
-        }
     }
 
-    if (dev == NULL) {
-        DOCA_LOG_ERR("No PCI export-capable device found");
+    result = open_device_representor(dev, &dev_rep);
+    if (result != DOCA_SUCCESS) {
+        DOCA_LOG_ERR("Failed to open device representor: %s",
+                     doca_error_get_descr(result));
         return EXIT_FAILURE;
     }
 
@@ -151,49 +145,93 @@ int main(void)
         return EXIT_FAILURE;
     }
 
-    struct export_file_header hdr;
     hdr.remote_addr = (uint64_t)(uintptr_t)buffer;
     hdr.remote_len = BUFFER_SIZE;
     hdr.export_desc_len = export_desc_len;
 
-    server_fd = create_server_socket();
-    if (server_fd < 0)
+    result = doca_comm_channel_ep_create(&ep);
+    if (result != DOCA_SUCCESS) {
+        DOCA_LOG_ERR("Failed to create comm channel endpoint: %s",
+                     doca_error_get_descr(result));
+        return EXIT_FAILURE;
+    }
+
+    result = doca_comm_channel_ep_set_device(ep, dev);
+    if (result != DOCA_SUCCESS)
         return EXIT_FAILURE;
 
-    DOCA_LOG_INFO("Waiting for BlueField connection on TCP port %d", TCP_PORT);
-
-    client_fd = accept(server_fd, NULL, NULL);
-    if (client_fd < 0) {
-        perror("accept");
+    result = doca_comm_channel_ep_set_device_rep(ep, dev_rep);
+    if (result != DOCA_SUCCESS)
         return EXIT_FAILURE;
+
+    result = doca_comm_channel_ep_listen(ep, SERVICE_NAME);
+    if (result != DOCA_SUCCESS) {
+        DOCA_LOG_ERR("Failed to listen on service: %s",
+                     doca_error_get_descr(result));
+        return EXIT_FAILURE;
+    }
+
+    DOCA_LOG_INFO("Waiting for BlueField connection using DOCA Comm Channel");
+
+    char hello[64];
+    size_t hello_len = sizeof(hello);
+
+    while (1) {
+        result = doca_comm_channel_ep_recvfrom(ep,
+                                               hello,
+                                               &hello_len,
+                                               DOCA_CC_MSG_FLAG_NONE,
+                                               &peer_addr);
+        if (result == DOCA_SUCCESS)
+            break;
+
+        if (result != DOCA_ERROR_AGAIN) {
+            DOCA_LOG_ERR("Failed while waiting for peer: %s",
+                         doca_error_get_descr(result));
+            return EXIT_FAILURE;
+        }
     }
 
     DOCA_LOG_INFO("BlueField connected");
+    DOCA_LOG_INFO("Sending export header");
 
-    if (send_all(client_fd, &hdr, sizeof(hdr)) < 0) {
-        perror("send header");
+    result = doca_comm_channel_ep_sendto(ep,
+                                         &hdr,
+                                         sizeof(hdr),
+                                         DOCA_CC_MSG_FLAG_NONE,
+                                         peer_addr);
+    if (result != DOCA_SUCCESS) {
+        DOCA_LOG_ERR("Failed to send header: %s", doca_error_get_descr(result));
         return EXIT_FAILURE;
     }
 
-    if (send_all(client_fd, export_desc, export_desc_len) < 0) {
-        perror("send export descriptor");
+    DOCA_LOG_INFO("Sending export descriptor");
+
+    result = doca_comm_channel_ep_sendto(ep,
+                                         export_desc,
+                                         export_desc_len,
+                                         DOCA_CC_MSG_FLAG_NONE,
+                                         peer_addr);
+    if (result != DOCA_SUCCESS) {
+        DOCA_LOG_ERR("Failed to send export descriptor: %s",
+                     doca_error_get_descr(result));
         return EXIT_FAILURE;
     }
 
-    DOCA_LOG_INFO("Export descriptor sent over TCP");
+    DOCA_LOG_INFO("Export descriptor sent");
     DOCA_LOG_INFO("Host buffer address: %p", buffer);
     DOCA_LOG_INFO("Host buffer length : %lu", hdr.remote_len);
-    DOCA_LOG_INFO("Keep this program running while BlueField performs DMA");
-    DOCA_LOG_INFO("Press Enter only after BlueField finishes");
+    DOCA_LOG_INFO("Keep host running while BlueField performs DMA");
+    DOCA_LOG_INFO("Press Enter after BlueField finishes");
 
     getchar();
 
-    close(client_fd);
-    close(server_fd);
+    doca_comm_channel_ep_disconnect(ep, peer_addr);
+    doca_comm_channel_ep_destroy(ep);
 
     doca_mmap_destroy(mmap);
+    doca_dev_rep_close(dev_rep);
     doca_dev_close(dev);
-    doca_devinfo_destroy_list(dev_list);
     free(buffer);
 
     return EXIT_SUCCESS;
